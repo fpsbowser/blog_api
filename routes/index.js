5aa9acf56d0ba51d7933cbf201ce9e43f8fa018e
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-11-22 07:55:07 -0500

    changed response from ejs to JSON

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // res.render("index", { title: "blog_api" });
+  res.json({
+    message:
+      'Welcome to blog_api! This is a RESTful API created for a mock blog following theodinproject curriculum.',
+  });
 });
 
 module.exports = router;
