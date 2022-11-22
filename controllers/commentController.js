a5870a04d8acb0755ad12318387131ce819a101e
Dakota <prattdakota12@gmail.com>
2022-11-21 21:05:45 -0500

    initial controller commit

---
+++ controllers/commentController.js
@@ -0,0 +1,154 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+const { body, validationResult } = require('express-validator');
+
+exports.comments_list = function (req, res, next) {
+  // Fetch all comments
+  // Return JSON
+  Comment.find({ post: req.params.id }).exec(function (err, list_comments) {
+    if (err) {
+      res
+        .json({
+          message: `Can't find post with id: ${req.params.id}`,
+          error: err,
+        })
+        .status(400);
+      return next(err);
+    }
+    res.json(list_comments);
+  });
+};
+
+exports.comment_detail = function (req, res, next) {
+  // Fetch specific comment
+  // Return JSON
+  Post.findById(req.params.id, (err, post) => {
+    if (err) {
+      res
+        .json({
+          message: `Can't find post with id: ${req.params.id}`,
+          error: err,
+        })
+        .status(400);
+      return next(err);
+    }
+    Comment.findById(req.params.commentid)
+      .populate('post')
+      .exec(function (err, results) {
+        if (err) {
+          return next(err);
+        }
+        res.json(results);
+      });
+  });
+};
+
+exports.comment_post = [
+  body('name', 'must provide name').trim().isLength({ min: 1 }).escape(),
+  body('comment', 'must provide a comment')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    Post.findById(req.params.id, (err, post) => {
+      if (err) {
+        res
+          .json({
+            message: `Can't find post with id: ${req.params.id}`,
+            error: err,
+          })
+          .status(400);
+        return next(err);
+      }
+      if (!errors.isEmpty()) {
+        res.json(errors);
+      } else {
+        const comment = new Comment({
+          name: req.body.name,
+          comment: req.body.comment,
+          timestamp: new Date(),
+          post: req.params.id,
+        });
+
+        comment.save((err) => {
+          if (err) {
+            next(err);
+          } else {
+            res.json({ status: 'Success', comment });
+          }
+        });
+      }
+    });
+  },
+];
+
+// Fetch specific comment
+exports.comment_delete = (req, res, next) => {
+  Comment.findByIdAndRemove(req.params.commentid, (err, comment) => {
+    if (err) {
+      return next(err);
+    }
+    console.log(
+      `Successfully removed comment with id: ${req.params.commentid}`
+    );
+    res.json({ status: 'Successfully removed comment', comment }).status(200);
+  });
+};
+
+exports.comment_update = [
+  // Validate and sanitize fields.
+  body('name', 'New name must be a string.').optional().isString(),
+  body('comment', 'New comment must be a string.').optional().isString(),
+  // Process request after validation and sanitization.
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+    const originalComment = await Comment.findById(req.params.commentid).exec();
+    console.log(originalComment.name);
+
+    Post.findById(req.params.id, (err, post) => {
+      if (err) {
+        res
+          .json({
+            message: `Can't find post with id: ${req.params.id}`,
+            error: err,
+          })
+          .status(400);
+        return next(err);
+      }
+
+      // Create a commemt object with provided data and old id/data.
+      const comment = new Comment({
+        _id: req.params.commentid, //This is required, or a new ID will be assigned!
+        name: req.body.name ? req.body.name : originalComment.name,
+        comment: req.body.comment ? req.body.comment : originalComment.comment,
+        timestamp: originalComment.timestamp,
+        edited_timestamp: new Date(),
+      });
+      if (!errors.isEmpty()) {
+        // There are errors.
+        res.json(errors).status(400);
+      } else {
+        // Data is valid. Update the record.
+        Comment.findByIdAndUpdate(
+          req.params.commentid,
+          comment,
+          { returnDocument: 'after' },
+          (err, updatedcomment) => {
+            if (err) {
+              return next(err);
+            }
+            // Successful: redirect to type detail page.
+            res.json({
+              status: 'Success',
+              old: originalComment,
+              updatedcomment,
+            });
+          }
+        );
+      }
+    });
+  },
+];
---
+++ controllers/postController.js
@@ -0,0 +1,151 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+const User = require('../models/user');
+const async = require('async');
+const { body, validationResult } = require('express-validator');
+
+exports.posts_list = function (req, res, next) {
+  // Fetch all posts
+  // Return JSON
+  Post.find({}).exec(function (err, list_posts) {
+    if (err) {
+      return next(err);
+    }
+    res.json(list_posts);
+  });
+};
+
+exports.post_detail = function (req, res, next) {
+  // Fetch specific post
+  // Return JSON
+  Post.findById(req.params.id).exec(function (err, results) {
+    if (err) {
+      return next(err);
+    }
+    res.json(results);
+  });
+};
+
+// Async parallel find post and all comments for post
+// Fetch specific post
+exports.post_delete = (req, res, next) => {
+  async.parallel(
+    {
+      post(callback) {
+        Post.findById(req.params.id).exec(callback);
+      },
+      post_comments(callback) {
+        Comment.find({ post: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      // Check to see if post has comments.
+      if (results.post_comments.length > 0) {
+        // Post has comments.
+        // Admin must delete all comments first
+        console.log(
+          'Failed to delete post because the post has comments! Please delete all comments before attempting to delete a post.'
+        );
+        res
+          .json({
+            status:
+              'Failed to delete post because the post has comments! Please delete all comments before attempting to delete a post.',
+            post_comments_count: results.post_comments.length,
+          })
+          .status(400);
+        return;
+      }
+      // Post has no comments. Delete object and send JSON response
+      Post.findByIdAndRemove(req.params.id, (err, post) => {
+        if (err) {
+          return next(err);
+        }
+        // Success
+        console.log(`Successfully removed post with id: ${post._id}`);
+        res.json({ status: 'Successfully removed post', post }).status(200);
+      });
+    }
+  );
+};
+
+exports.post_create = [
+  body('title', 'must provide title').trim().isLength({ min: 1 }),
+  body('message', 'must provide message').trim().isLength({ min: 1 }),
+  body('owner', 'must provide ID of owner')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('ispublic', 'must specify true or false')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.json(errors);
+    } else {
+      const post = new Post({
+        title: req.body.title,
+        message: req.body.message,
+        timestamp: new Date(),
+        // search by username?
+        owner: req.body.owner,
+        ispublic: req.body.ispublic,
+      });
+
+      post.save((err) => {
+        if (err) {
+          next(err);
+        } else {
+          res.json({ success: 'true', post });
+        }
+      });
+    }
+  },
+];
+
+exports.post_update = [
+  // Validate and sanitize fields.
+  body('title', 'New title must be a string.').optional().isString(),
+  body('message', 'New message must be a string.').optional().isString(),
+  body('ispublic', 'Must provide boolean value.').optional().isBoolean(),
+  // Process request after validation and sanitization.
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+    const originalPost = await Post.findById(req.params.id).exec();
+    console.log(originalPost.title);
+    // Create a post object with provided data and old id/data.
+    const post = new Post({
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+      title: req.body.title ? req.body.title : originalPost.title,
+      message: req.body.message ? req.body.message : originalPost.message,
+      timestamp: originalPost.timestamp,
+      owner: originalPost.owner,
+      ispublic: req.body.ispublic ? req.body.ispublic : originalPost.ispublic,
+      edited_timestamp: new Date(),
+    });
+    if (!errors.isEmpty()) {
+      // There are errors.
+      res.json(errors).status(400);
+    } else {
+      // Data is valid. Update the record.
+      Post.findByIdAndUpdate(
+        req.params.id,
+        post,
+        { returnDocument: 'after' },
+        (err, updatedpost) => {
+          if (err) {
+            return next(err);
+          }
+          // Successful: redirect to type detail page.
+          res.json({ status: 'Success', old: originalPost, updatedpost });
+        }
+      );
+    }
+  },
+];
